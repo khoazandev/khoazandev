47271888a98ca6fa97ac790bfd083fb3813c3878
khoaznday <levankhoa2004@gmail.com>
2026-04-07 13:23:56 +0700

    fix: update basePath to /khoazandev

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/1dao2mang",
+  basePath: "/khoazandev",
   images: {
     unoptimized: true,
   },
